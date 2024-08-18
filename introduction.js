5f38ce114c793064da1dc128381f944584732c3f
amit_kun <109193883+amitkrkun@users.noreply.github.com>
2024-08-18 08:22:31 +0530

    intro

---
+++ introduction.js
@@ -0,0 +1,36 @@
+// Variable --->  used to store variable
+
+// Dynamic type == can change value at runtime
+// statically type == already have to declare value
+// javascript is a dynamic type language
+
+//  -->  const, let, var
+// var is globally scoped while let & const are blocked scope 
+// var can be updated and redeclared within the scope
+// const can't be updated not redeclared
+// let is mostly used to declare block scoped variable
+// let can be updated but not redeclared
+
+
+let x = 50;
+let y = "amit"
+let z = undefined
+let xy = null
+
+// rules for choosing variable name
+
+// letters, digits, underscores & $sign allowd
+// must begin with a $, _. or a letter
+// javascript is case sensitive
+// Javascript reserved words cannot be used as a variable name
+// Variable & variable are differnent variable
+
+
+
+
+console.log("javascript")
+
+
+
+
+
