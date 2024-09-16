8ba4b5e6e7e031a832958d508c5f99c6712c67ba
amit_kun <109193883+amitkrkun@users.noreply.github.com>
2024-09-16 23:45:36 +0530

    dvf

--- my.sql
+++ my.sql
@@ -44,3 +44,81 @@ select * from train where afe>10 and age<15
 select   * from train where age between 10 and 15
 
 
+select title from movies where genre like 'comedy ' or genet like 'action'
+
+
+select title.genre from moviw where genre in ("action", "horror, " drama")
+
+
+select title from movies table where title like "A%"
+
+select title from movies where title like "%man%"
+
+
+
+select title from movie where actor like '%khan%' or actor like '%kapoo%'
+
+select title from movies where title like '_____'
+
+update passenger set name = 'rahul'
+
+update passenger set name = 'rohi' where email like '&gmail%
+
+update passenger set  name = 'ankit', email = 'abc@gmail.com' where email like '%yahoo%'
+
+delete from passenger where id = 1
+
+
+delete from movies where id > 2 and email like '%yahoo%'
+
+delete from passenger where 1
+
+---- function 
+-- 
+
+
+select title, (india_gross - profit) as profit from movie
+
+
+
+select title, abs((india_gross - profit)) as profit from movies;
+
+
+select title, round((runtime/60), 1) as runtime_hours from movies
+
+
+select tile, ceil(runtime/60) as runtime_hourd from movies
+
+
+select tile, floor(runtime/60) as runtime_hourd from movies
+
+
+select upper(title) from movies
+
+select title, concat(actor, '', director ) as crew from movies
+
+
+select title, length(title ) as length from movies,
+
+
+select title, Substr(title, 1, 5) as short from movies
+
+
+select max(budget) from movies
+
+
+select min(earned) from movies
+
+select sum(india_gross) from movies
+
+select avg(india_gross) from movies
+
+select count(*) from movies
+
+select actor, count(distinct(actor))  as movie_count from mobvies
+
+
+select title, (world wide - budget) as profit from movies order by profit desc linit 5
+
+
+select * from movies order by genre, title 
