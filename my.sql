7a41a6d58a538bd6a38137643610daa45ea7fc0a
amit_kun <109193883+amitkrkun@users.noreply.github.com>
2024-09-16 21:10:32 +0530

    rwv

---
+++ my.sql
@@ -0,0 +1,46 @@
+insert into students(sid, sname, college, )values(4, "nitish,/;aamit@gmail.com,"col)
+
+alter table students modify column snamevarchar(255)
+
+insert into students (5, "rahul, "abviiit@gmail.com","itm)
+
+insert into students vlues (),(),()
+
+select * from trains
+-- give all the data from the column
+
+
+select name, sex , surviveds from table
+
+select name as passangename, sex as gender, survived from trains
+
+
+select name , age + 102 as Current age from ttrain,
+
+
+select name, 10000 as compensation from trains
+
+
+select distinct sex from trains
+
+
+select distinct embarked from trains
+
+select distinct pclass from trains
+
+select distinct pclass, embarked from train
+
+
+selct * from train wher survived = 0
+select * from train where pclass = 3
+
+select * from tain where age>50
+
+select * from train where pclass = 3 and survived = 0
+
+select * from train where afe>10 and age<15
+\
+
+select   * from train where age between 10 and 15
+
+
